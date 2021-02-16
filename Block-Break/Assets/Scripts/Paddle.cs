716991042bb1c3f16ea50788b68e01066acbc585
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-02-16 15:54:40 -0500

    added paddle movement from left to right on x axis

---
+++ Block-Break/Assets/Scripts/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public Camera mainCamera;
+    void Start()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+   
+    void Update()
+    {
+        transform.position = new Vector3(mainCamera.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, 0, 0)).x, -4, 0);
+
+    }
+}
