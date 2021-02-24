a95900444792d49fff1344e451bbb1c842e412c7
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-02-24 17:45:01 -0500

    build the game

---
+++ Block-Break/Assets/Scripts/Restart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+
+     public void Prevscene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+}
+
+
+
+
