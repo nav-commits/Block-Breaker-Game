cbca8a9693507a6aed727bb6315b9e8655da2a0a
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-02-25 15:33:55 -0500

    added life logic

--- Block-Break/Assets/Scripts/EndGame.cs
+++ Block-Break/Assets/Scripts/EndGame.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndGame : MonoBehaviour
 {
+    public Text Lives;
+    public int lifes;
+  
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        SceneManager.LoadScene(1);
 
+        //Life();
+
+        if (lifes == 0)
+        {
+            SceneManager.LoadScene(1);
+            Life();
+        }
+        
+    }
+
+    public void Life()
+    {
+        lifes--;
+        Lives.text = "lives" + lifes.ToString();
     }
 
 
